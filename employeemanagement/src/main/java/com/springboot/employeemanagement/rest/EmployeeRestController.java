1b11c78ba9b6a0a9c02c69e2df30d2d92e8429d8
erandakaanupama <erandakaanupama@gmail.com>
2019-05-25 23:52:45 +0530

    rest controller updated

--- employeemanagement/src/main/java/com/springboot/employeemanagement/rest/EmployeeRestController.java
+++ employeemanagement/src/main/java/com/springboot/employeemanagement/rest/EmployeeRestController.java
@@ -2,10 +2,9 @@ package com.springboot.employeemanagement.rest;
 
 import com.springboot.employeemanagement.dao.EmployeeDAO;
 import com.springboot.employeemanagement.entity.Employee;
+import com.springboot.employeemanagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +16,40 @@ import java.util.List;
 public class EmployeeRestController {
 
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @GetMapping("/employees")
     public List<Employee> getEmployees(){
-        return employeeDAO.findAll();
+        return employeeService.findAll();
+    }
+
+    // get employee
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        return employeeService.findById(employeeId);
+    }
+
+    // add an employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+        this.employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    //update employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        this.employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    // delete an employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tmpEmployee = employeeService.findById(employeeId);
+        this.employeeService.deleteEmployee(employeeId);
+
+        return "deleted employee id: " + employeeId;
     }
 }
