ac99bd7c354c0f6a407c9589ea407fedc8b8add7
erandakaanupama <erandakaanupama@gmail.com>
2019-05-25 21:51:33 +0530

    controller added for find all employees

--- employeemanagement/src/main/java/com/springboot/employeemanagement/rest/EmployeeRestController.java
+++ employeemanagement/src/main/java/com/springboot/employeemanagement/rest/EmployeeRestController.java
@@ -1,7 +1,26 @@
 package com.springboot.employeemanagement.rest;
 
+import com.springboot.employeemanagement.dao.EmployeeDAO;
+import com.springboot.employeemanagement.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author erandaka_b
  */
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(){
+        return employeeDAO.findAll();
+    }
 }
