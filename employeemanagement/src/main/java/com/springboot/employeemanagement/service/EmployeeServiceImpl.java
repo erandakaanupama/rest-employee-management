085a6fd2faa290a295a6cdaee30b4d80c11f99a1
erandakaanupama <erandakaanupama@gmail.com>
2019-05-25 22:47:54 +0530

    service implementations added

--- employeemanagement/src/main/java/com/springboot/employeemanagement/service/EmployeeServiceImpl.java
+++ employeemanagement/src/main/java/com/springboot/employeemanagement/service/EmployeeServiceImpl.java
@@ -1,7 +1,43 @@
 package com.springboot.employeemanagement.service;
 
+import com.springboot.employeemanagement.dao.EmployeeDAO;
+import com.springboot.employeemanagement.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @author erandaka_b
  */
-public class EmployeeServiceImpl {
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return this.employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int id) {
+        return this.employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        this.employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        this.employeeDAO.deleteById(id);
+    }
 }
