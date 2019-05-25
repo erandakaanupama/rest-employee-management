34a6ec948fe1ea2c0c949618beac20cc4d9ecd04
erandakaanupama <erandakaanupama@gmail.com>
2019-05-25 22:39:37 +0530

    service methods added

--- employeemanagement/src/main/java/com/springboot/employeemanagement/service/EmployeeService.java
+++ employeemanagement/src/main/java/com/springboot/employeemanagement/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package com.springboot.employeemanagement.service;
 
+import com.springboot.employeemanagement.entity.Employee;
+
+import java.util.List;
+
 /**
  * @author erandaka_b
  */
 public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    void saveEmployee(Employee employee);
+
+    void deleteEmployee(int id);
 }
